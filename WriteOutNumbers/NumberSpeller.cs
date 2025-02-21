8e618126232b29b0ddefe2f3e590433456d3dc1e
TorLindstrom <tor.lindstrom@winlas.se>
2025-02-21 09:40:31 +0100

    refactor: inline method

--- WriteOutNumbers/NumberSpeller.cs
+++ WriteOutNumbers/NumberSpeller.cs
@@ -11,7 +11,16 @@ public abstract class NumberSpeller
 	{
 		return number switch
 		{
-			>= 0 and <= 9 => SpellOutSingleDigit(number),
+			0 => "zero",
+			1 => "one",
+			2 => "two",
+			3 => "three",
+			4 => "four",
+			5 => "five",
+			6 => "six",
+			7 => "seven",
+			8 => "eight",
+			9 => "nine",
 			10 => "ten",
 			11 => "eleven",
 			12 => "twelve",
@@ -42,27 +51,12 @@ public abstract class NumberSpeller
 		{
 			return "";
 		}
+
 		if (shouldAppendHyphen)
 		{
-			return $"-{SpellOutSingleDigit(number)}";
+			return $"-{SpellNumberWithMultipleDigits(number)}";
 		}
-		return $" {SpellNumberWithMultipleDigits(number)}";
-	}
 
-	private static string SpellOutSingleDigit(int number)
-	{
-		return number switch
-		{
-			0 => "zero",
-			1 => "one",
-			2 => "two",
-			3 => "three",
-			4 => "four",
-			5 => "five",
-			6 => "six",
-			7 => "seven",
-			8 => "eight",
-			9 => "nine"
-		};
+		return $" {SpellNumberWithMultipleDigits(number)}";
 	}
 }
