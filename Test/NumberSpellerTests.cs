86e0c93f5f43f099117349c2dadea1baf8a5355b
TorLindstrom <tor.lindstrom@winlas.se>
2025-02-21 08:55:48 +0100

    feat: 10..20 -> 'ten'..'twenty'

--- Test/NumberSpellerTests.cs
+++ Test/NumberSpellerTests.cs
@@ -4,23 +4,34 @@ namespace Test;
 
 public class NumberSpellerTests
 {
-    [Theory(DisplayName = "Inputting a number should spell out that number")]
-    [InlineData(0, "zero")]
-    [InlineData(1, "one")]
-    [InlineData(2, "two")]
-    [InlineData(3, "three")]
-    [InlineData(4, "four")]
-    [InlineData(5, "five")]
-    [InlineData(6, "six")]
-    [InlineData(7, "seven")]
-    [InlineData(8, "eight")]
-    [InlineData(9, "nine")]
-    public void GivenANumber_ShouldSpellOutTheNumber(int number, string? expectedString)
-    {
-        // act
-        var result = NumberSpeller.SpellOut(number);
-        
-        // assert
-        Assert.Equal(expectedString, result);
-    }
+	[Theory(DisplayName = "Inputting a number should spell out that number")]
+	[InlineData(0, "zero")]
+	[InlineData(1, "one")]
+	[InlineData(2, "two")]
+	[InlineData(3, "three")]
+	[InlineData(4, "four")]
+	[InlineData(5, "five")]
+	[InlineData(6, "six")]
+	[InlineData(7, "seven")]
+	[InlineData(8, "eight")]
+	[InlineData(9, "nine")]
+	[InlineData(10, "ten")]
+	[InlineData(11, "eleven")]
+	[InlineData(12, "twelve")]
+	[InlineData(13, "thirteen")]
+	[InlineData(14, "fourteen")]
+	[InlineData(15, "fifteen")]
+	[InlineData(16, "sixteen")]
+	[InlineData(17, "seventeen")]
+	[InlineData(18, "eighteen")]
+	[InlineData(19, "nineteen")]
+	[InlineData(20, "twenty")]
+	public void GivenANumber_ShouldSpellOutTheNumber(int number, string? expectedString)
+	{
+		// act
+		var result = NumberSpeller.SpellOut(number);
+
+		// assert
+		Assert.Equal(expectedString, result);
+	}
 }
